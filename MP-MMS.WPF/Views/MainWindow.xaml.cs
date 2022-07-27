1269f09d730ed5c4666129bd39530c9638ba8e77
Zadok Joshua <ayegbazadok@outlook.com>
2022-07-27 15:31:21 +0100

    Added Pages folder

--- MP-MMS.WPF/Views/MainWindow.xaml.cs
+++ MP-MMS.WPF/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MP_MMS.WPF.Views.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +24,8 @@ namespace MP_MMS.WPF
         public MainWindow()
         {
             InitializeComponent();
+            ContentFrame.Content = new HomeDashboard();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
--- MP-MMS.WPF/Views/Pages/HomeDashboard.xaml.cs
+++ MP-MMS.WPF/Views/Pages/HomeDashboard.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MP_MMS.WPF.Controls
+namespace MP_MMS.WPF.Views.Pages
 {
     /// <summary>
-    /// Interaction logic for NavBar.xaml
+    /// Interaction logic for HomeDashboard.xaml
     /// </summary>
-    public partial class NavBar : UserControl
+    public partial class HomeDashboard : Page
     {
-        public NavBar()
+        public HomeDashboard()
         {
             InitializeComponent();
         }
