8d399aaebe9b29a5b6534234903045b594733115
Zadok J <ayegbazadok@outlook.com>
2023-06-30 16:47:00 +0100

    Update Pages

--- MP-MMS.WPF/Views/Pages/HomeDashboard.xaml.cs
+++ MP-MMS.WPF/Views/Pages/HomeDashboard.xaml.cs
@@ -1,43 +1,59 @@
-﻿using MP_MMS.WPF.ChartResources;
+﻿using MP_MMS.Data;
+using MP_MMS.Domain.Model;
+using MP_MMS.WPF.ChartResources;
 using Syncfusion.UI.Xaml.Charts;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using PieSeries = Syncfusion.UI.Xaml.Charts.PieSeries;
 
-namespace MP_MMS.WPF.Views.Pages
+namespace MP_MMS.WPF.Views.Pages;
+
+/// <summary>
+/// Interaction logic for HomeDashboard.xaml
+/// </summary>
+public partial class HomeDashboard : Page
 {
-    /// <summary>
-    /// Interaction logic for HomeDashboard.xaml
-    /// </summary>
-    public partial class HomeDashboard : Page
+
+
+    public HomeDashboard()
     {
+        InitializeComponent();
 
-        public HomeDashboard()
+        GetSummaryData();
+
+        ViewModel viewModel = new ViewModel();
+        PieChartDisplay.DataContext = viewModel;
+        PieChartDisplay.Header = "Issues Report";
+        PieChartDisplay.Legend = new ChartLegend()
         {
-            InitializeComponent();
-            ViewModel viewModel = new ViewModel();
-            PieChartDisplay.DataContext = viewModel;
-            PieChartDisplay.Header = "Issues Report";
-            PieChartDisplay.Legend = new ChartLegend()
-            {
-                DockPosition = ChartDock.Bottom,
-                IconHeight = 20,
-                IconWidth = 20
-            };
+            DockPosition = ChartDock.Bottom,
+            IconHeight = 20,
+            IconWidth = 20
+        };
 
-            // Defining the PieSeries with adornments.
-            PieSeries series = new PieSeries();
-            series.XBindingPath = "Name";
-            series.YBindingPath = "Count";
-            series.ItemsSource = viewModel.Data;
-            series.Palette = ChartColorPalette.GreenChrome;
-            
-            series.AdornmentsInfo = new ChartAdornmentInfo()
-            {
-                ShowLabel = true
-            };
-            PieChartDisplay.Series.Add(series);
+        // Defining the PieSeries with adornments.
+        PieSeries series = new PieSeries();
+        series.XBindingPath = "Name";
+        series.YBindingPath = "Count";
+        series.ItemsSource = viewModel.Data;
+        series.Palette = ChartColorPalette.GreenChrome;
+        
+        series.AdornmentsInfo = new ChartAdornmentInfo()
+        {
+            ShowLabel = true
+        };
+        PieChartDisplay.Series.Add(series);
+    }
+
+    public void GetSummaryData()
+    {
+        using (MPMMSDbContext context = new())
+        {
+            TbOpenIssues.Text = context.Issues.Where(i => i.IsCompleted.Equals(false)).Count().ToString();
+            TbMachineParts.Text = context.Parts.Count().ToString();
+            TbEmployees.Text = context.Employees.Count().ToString();
         }
     }
 }
